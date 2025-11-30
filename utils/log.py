901207194805933518dad2bfe384543775629086
Daniele Angeli <daniele.angeli@molo17.com>
2025-11-30 21:38:47 +0100

    use proxy

--- utils/log.py
+++ utils/log.py
@@ -101,7 +101,7 @@ def add_handlers(logger: logging.Logger, log_file=None):
     if is_docker:
         try:
             # Add Logstash handler only in Docker container
-            logstash_host = '10.17.3.230'
+            logstash_host = '10.17.3.107'
             logstash_port = 5000
             
             # Initialize Logstash handler with error handling
