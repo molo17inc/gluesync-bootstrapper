5f46f8809f85495d2574b8b84fa62f2c051b3ea2
Daniele Angeli <daniele.angeli@molo17.com>
2026-03-09 09:46:41 +0100

    fixed snapshot write method

--- main.py
+++ main.py
@@ -772,20 +772,14 @@ if True:
             entityId = entity['entityId']
             entityName = entity['entityName']
             
-            # TODO: Implement snapshotWriteMethod when API supports it
-            # Currently the API doesn't accept snapshotWriteMethod as a query parameter
-            # Get the snapshotWriteMethod from YAML configuration
-            # snapshot_write_method = get_snapshot_write_method_from_yaml(entityName, yaml_path)
-            # if snapshot_write_method != 'UPSERT':
-            #     logger.info(f"Using snapshotWriteMethod '{snapshot_write_method}' for entity {entityName}")
+            snapshot_write_method = get_snapshot_write_method_from_yaml(entityName, yaml_path)
+            if snapshot_write_method != 'UPSERT':
+                logger.info(f"Using snapshotWriteMethod '{snapshot_write_method}' for entity {entityName}")
 
             try:
                 encoded_entity_id = safe_encode(entityId)
                 
-                # Build query parameters - currently only with entity ID
-                # TODO: Add snapshotWriteMethod when API supports it
-                query_params = f"entity={encoded_entity_id}"
-                # Future: query_params = f"entity={encoded_entity_id}&snapshotWriteMethod={snapshot_write_method}"
+                query_params = f"entity={encoded_entity_id}&snapshotWriteMethod={snapshot_write_method}"
 
                 response = fetch_core_hub(
                     f"/pipelines/{pipeline_id}/commands/sync/start?withSnapshot=true&{query_params}",
